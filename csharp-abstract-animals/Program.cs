5cb1eb5e2118f715541f5dcee07db3fc162f449c
sirio95 <sirio.iasevoli@gmail.com>
2023-04-13 16:28:00 +0200

    esercizio2

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -1,29 +1,73 @@
-﻿namespace csharp_abstract_animals
+﻿using System.Diagnostics.Contracts;
+
+namespace csharp_abstract_animals
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+
+            Esercizio1();
+            Esercizio2();
+
+
+            
+
+
+        }
+        public static void Esercizio1() 
         {
             Aquila AquilaDiBonelli = new Aquila();
             AquilaDiBonelli.Dormi();
             AquilaDiBonelli.Verso();
             AquilaDiBonelli.Mangia();
 
+
             Cane Bassotto = new Cane();
             Bassotto.Dormi();
             Bassotto.Verso();
             Bassotto.Mangia();
 
+
+
             Delfino StenellaMaculata = new Delfino();
             StenellaMaculata.Dormi();
             StenellaMaculata.Verso();
             StenellaMaculata.Mangia();
-            
+
+
             Passerotto Testagrigia = new Passerotto();
             Testagrigia.Dormi();
             Testagrigia.Verso();
             Testagrigia.Mangia();
+        }
+        public static void Esercizio2()
+        {
+            void FaiVolare(IVolante animal)
+            {
+                animal.Vola();
+            }
+            void FaiNuotare(INuotante animal)
+            {
+                animal.Nuota();
+            }
+            void FaiCamminare(ICamminante animal)
+            {
+                animal.Cammina();
+            }
 
+            for(int i = 0; i < 3; i++) 
+            {
+                Aquila aquila = new Aquila();
+                FaiVolare(aquila);
+                Passerotto Passero = new Passerotto();
+                FaiVolare(Passero);
+                Delfino delfino = new Delfino();
+                FaiNuotare(delfino);
+                Cane cane = new Cane();
+                FaiCamminare(cane);
+
+            }
 
         }
 
@@ -35,10 +79,9 @@
             }
             public abstract void Verso();
 
-            public abstract void Mangia();
-            
+            public abstract void Mangia();            
         }
-        public class Aquila : Animale
+        public class Aquila : Animale, IVolante, ICamminante
         {
             public override void Verso()
             {
@@ -48,8 +91,16 @@
             {
                 Console.WriteLine("pesci, piccoli e medi vertebrati");
             }
+            public void Vola()
+            {
+                Console.WriteLine("Sto Volando!");
+            }
+            public void Cammina()
+            {
+                Console.WriteLine("Posso anche camminare");
+            }
         }
-        public class Cane: Animale
+        public class Cane: Animale, ICamminante
         {
             public override void Verso()
             {
@@ -59,8 +110,12 @@
             {
                 Console.WriteLine("Onnivoro. Dieta al 50% composto da proteine animali");
             }
+            public void Cammina()
+            {
+                Console.WriteLine("Sto camminando!");
+            }
         }
-        public class Delfino: Animale
+        public class Delfino: Animale, INuotante
         {
             public override void Verso()
             {
@@ -70,8 +125,12 @@
             {
                 Console.WriteLine("pesci");
             }
+            public void Nuota()
+            {
+                Console.WriteLine("Sto nuotando");
+            }
         }
-        public class Passerotto: Animale
+        public class Passerotto: Animale, IVolante, ICamminante
         {
             public override void Verso()
             {
@@ -81,6 +140,37 @@
             {
                 Console.WriteLine("Onnivoro. Insetti e semi");
             }
+            public void Vola()
+            {
+                Console.WriteLine("Sto volando!");
+            }
+            public void Cammina()
+            {
+                Console.WriteLine("Posso anche camminare!");
+            }
         }
+
+        public interface IVolante
+        {
+            public void Vola();
+            
+        }
+        public interface INuotante
+        {
+            public void Nuota();
+            
+        }
+        public interface ICamminante
+        {
+            public void Cammina();
+            
+        }
+
+        
+
+        
+        
     }
+    
+    
 }
